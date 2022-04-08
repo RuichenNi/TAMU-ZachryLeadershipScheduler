c04cd90b584db07cc009306406c0a19081e7d438
Ubuntu <ubuntu@ip-172-31-39-140.ec2.internal>
2022-04-08 20:42:51 +0000

    add column Reason on add schedule UI and add reason attribute in table ScheduleToCourses

--- zlp-scheduler/db/schema.rb
+++ zlp-scheduler/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_000143) do
+ActiveRecord::Schema.define(version: 2022_04_08_203845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2022_04_07_000143) do
     t.string "start_time"
     t.string "end_time"
     t.integer "day", default: 1
+    t.string "reason"
     t.index ["course_id"], name: "index_schedule_to_courses_on_course_id"
     t.index ["schedule_id"], name: "index_schedule_to_courses_on_schedule_id"
   end
